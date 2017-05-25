195476074efefad4bddd080a816f073bba76abd2
jhchoi <cjh880118@gmail.com>
2017-05-25 11:15:54 +0900

    xFileLoader추가

---
+++ 10DX3D00/cXFileLoader.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "cXFileLoader.h"
+
+
+cXFileLoader::cXFileLoader()
+{
+}
+
+
+cXFileLoader::~cXFileLoader()
+{
+}
---
+++ 10DX3D00/cXFileLoader.h
@@ -0,0 +1,8 @@
+#pragma once
+class cXFileLoader
+{
+public:
+	cXFileLoader();
+	~cXFileLoader();
+};
+
